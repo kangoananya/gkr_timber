fa365eb809a1d1f334dd77eac5a02e2d30fb87fc
ananya <kango@arch.ethz.ch>
2024-11-01 12:44:11 +0100

    update move.py

--- move.py
+++ move.py
@@ -5,14 +5,22 @@ ros = rrc.RosClient()
 ros.run()
 
 abb = rrc.AbbClient(ros, '/rob12')
-abb.send(rrc.SetTool('T_A083_MyTool'))
+abb.send(rrc.SetTool('T_A073_Trowel'))
+
 # Move robot to a joints
 frame, external_axes = abb.send_and_wait(rrc.GetRobtarget())
-print(frame, external_axes)
-frame.point.x += 100
-# done = abb.send_and_wait(rrc.MoveToRobtarget(frame, external_axes, 150.0, rrc.Zone.FINE))
 
-print(frame, external_axes)
-# Close clientsni
+# Move robot to home position
+joints, external_axes = abb.send_and_wait(rrc.GetJoints())
+done = abb.send_and_wait(rrc.MoveToJoints(joints=joints, external_axes=external_axes, speed=150.0, zone=rrc.Zone.FINE))
+
+# Move robot to a list of frames (future from JSON)
+frames = [frame]
+for f in frames:
+    # Can implement logic to update external axes here
+    done = abb.send_and_wait(rrc.MoveToRobtarget(frame, external_axes, 150.0, rrc.Zone.FINE))
+
+
+# Close clients
 ros.close()
 ros.terminate()
